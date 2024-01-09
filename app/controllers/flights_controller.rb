b6d0b008576cc6355c90794ae0427242c8f3aa0d
dotaplayer1 <georgemao72@gmail.com>
2024-01-09 12:38:28 -0800

    Improve search form

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,5 +2,14 @@ class FlightsController < ApplicationController
   def index
     @flights = Flight.all
     @airports = Airport.all
+
+    @searched_flights = Flight.all.order(:date)
+    @searched_flights = @searched_flights.during_date(flight_search_params[:date]) unless flight_search_params[:date] == ""
+    @searched_flights = @searched_flights.from_airport(flight_search_params[:departure_code]) unless flight_search_params[:departure_code] == ""
+    @searched_flights = @searched_flights.to_airport(flight_search_params[:arrival_code]) unless flight_search_params[:arrival_code] == ""
+  end
+
+  def flight_search_params
+    params.permit(:departure_code, :arrival_code, :date, :num_tickets)
   end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,8 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: "Airport"
   belongs_to :arrival_airport, class_name: "Airport"
+
+  scope :during_date, ->(date) { where(date: date) }
+  scope :from_airport, ->(departure_airport_id) { where(departure_airport: departure_airport_id) }
+  scope :to_airport, ->(arrival_airport_id) { where(arrival_airport: arrival_airport_id) }
 end
